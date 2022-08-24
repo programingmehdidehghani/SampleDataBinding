f81aaa95775feb903221baf56687282df2174671
mehdi <dehghani_m@rocketmail.com>
2022-08-24 21:58:36 +0430

    make class model

--- app/src/main/java/com/example/databinding/MyBindingAdapter.kt
+++ app/src/main/java/com/example/databinding/MyBindingAdapter.kt
@@ -1,2 +1,12 @@
 package com.example.databinding
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+
+
+@BindingAdapter("loadImageFromUrl")
+fun ImageView.loadImageFromUrl(profilePhoto : String){
+
+    this.load(profilePhoto)
+}
